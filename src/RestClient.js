e4fa6b7e7fd28d093f5deb2f10719c68e8704912
Denys Babii <denys.babii@gmail.com>
2016-10-23 18:22:20 +0300

    Implemented rest client with callbacks

---
+++ src/RestClient.js
@@ -0,0 +1,17 @@
+export default class RestClient {
+
+    static fetchJson(path, resolveHandler, rejectHandler) {
+        let request = new XMLHttpRequest();
+        request.open('GET', path);
+        request.onreadystatechange = () => {
+            if (request.readyState == XMLHttpRequest.DONE) {
+                if (request.status !== 200) {
+                    rejectHandler(Error(`Failed to load [${path}] with response code ${request.status}`));
+                    return;
+                }
+                resolveHandler(JSON.parse(request.responseText));
+            }
+        }
+        request.send();
+    }
+}
