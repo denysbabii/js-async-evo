c2ddad59afabc826b03248fe1603a2da5e16836e
Denys Babii <denys.babii@gmail.com>
2016-10-23 20:09:37 +0300

    Gone out of XMLHttpRequest to fetch

--- src/RestClient.js
+++ src/RestClient.js
@@ -1,19 +1,11 @@
 export default class RestClient {
 
     static fetchJson(path) {
-        return new Promise((resolve, reject) => {
-            let request = new XMLHttpRequest();
-            request.open('GET', path);
-            request.onreadystatechange = () => {
-                if (request.readyState == XMLHttpRequest.DONE) {
-                    if (request.status !== 200) {
-                        reject(Error(`Failed to load [${path}] with response code ${request.status}`));
-                        return;
-                    }
-                    resolve(JSON.parse(request.responseText));
-                }
-            }
-            request.send();
-        });
+        return fetch(path)
+            .then(response => {
+                return (response.status >= 200 && response.status < 300) ?
+                    Promise.resolve(response) :
+                    Promise.reject(new Error(`Path [${path}] is ${response.statusText}`));
+            }).then(response => response.json());
     }
 }
