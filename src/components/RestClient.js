46def5bf06601f9232a85f6e6f17275fae30c2bb
Denys Babii <denys.babii@gmail.com>
2016-11-13 16:17:45 +0200

    Improved client

--- src/components/RestClient.js
+++ src/components/RestClient.js
@@ -3,9 +3,9 @@ export default class RestClient {
     static fetchJson(path) {
         return fetch(path)
             .then(response => {
-                return (response.status >= 200 && response.status < 300) ?
+                return (response.ok) ?
                     Promise.resolve(response) :
-                    Promise.reject(new Error(`Path [${path}] is ${response.statusText}`));
+                    Promise.reject(new Error(`Path [${path}] is ${response.statusText}`)); //throw new Error() is the same
             }).then(response => response.json());
     }
 }
