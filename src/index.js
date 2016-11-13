62738fd9a1a10716731021a23ebf4477f388c519
Denys Babii <denys.babii@gmail.com>
2016-11-13 15:04:55 +0200

    Did a couple improvements

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
-// http://localhost:8989/api/languages/hz
-ReactDOM.render(
-    <App path="http://localhost:8989/api/languages" />,
-    document.getElementById("root")
-);
+
+window.onload = () => {
+    // http://localhost:8989/api/languages/hz
+    ReactDOM.render( 
+        <App path = "http://localhost:8989/api/languages" />,
+        document.getElementById("root")
+    );
+};
